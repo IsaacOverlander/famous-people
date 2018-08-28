55ffc28faf49737aacddb2defae5a57cbe3dcb85
Isaac Overlander <Isaac.overlander@gmail.com>
2018-08-28 15:11:12 -0500

    changed logo and header color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
 class Header extends Component {
     render() {
         return (
             <header className="App-header">
                 <h1 className="App-title">Welcome to Famous People</h1>
+                <img src="https://www.wwrebels.org/vimages/shared/vnews/stories/5890b551cbde5/1_gold-star-png--new-calendar-template-site-7.png" className="App-logo" alt=""/>
             </header>
         )
     }

0252e24f64f003ea33c6bc741422128f8b22d7c2
Isaac Overlander <Isaac.overlander@gmail.com>
2018-08-28 14:33:26 -0500

    Created Stars component

---
+++ src/components/Stars/Stars.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Stars extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            role: ''
+        }
+    }
+}
+
+export default Stars;
